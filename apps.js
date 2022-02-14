35a458bc0c5e459719add6f5407c2eb657bf5aa2
SüheyL <h.suheylyazici@gmail.com>
2022-02-14 23:53:38 +0300

    Js tamamlandı

--- apps.js
+++ apps.js
@@ -5,15 +5,37 @@ today.innerText = day.toLocaleString('en-us', { weekday: 'long' });
 
 let toText = document.getElementById("to__do--text");
 let toButton = document.getElementById("to__do__button");
-let toDo = document.getElementById("add_list");
+let toDo = document.getElementById("lists");
+let addList = document.getElementById("task__list");
 
-toButton.addEventListener('click', () => {
-    let addList = `<input type="checkbox" name="ch" id="ch">
-    <label style="color: white;" for="ch" id="textch">${toText.value}</label> <br>`;
-    toDo.innerHTML += addList;
+
+
+toButton.addEventListener("click", ()=> {
+    const li = document.createElement('li');
+    const input = document.createElement("input");
+    const label = document.createElement("label");
+    const del_icon = document.createElement("a");
+
+    del_icon.appendChild(document.createTextNode("✖"));
+
+    li.className = 'list-group-item';
+    del_icon.className="del__icon"
+    input.className = "input_item";
+    input.setAttribute("type", "checkbox");
+    input.id = "süheyl";
+    label.setAttribute("for","süheyl");
+    label.setAttribute("id","mehmet");
+    label.appendChild(document.createTextNode(toText.value));
+    li.appendChild(input);
+    li.appendChild(label)
+    addList.appendChild(li);
+    toText.value = "";
+    li.appendChild(del_icon);
+
+
+    del_icon.addEventListener("click", (e)=>{
+        e.target.parentElement.remove();
+    });
+    
 });
 
-let checkBoxT = document.getElementById("textch");
-checkBoxT.addEventListener("click", ()=> {
-    checkBoxT.innerHTML += <del>add_list</del>
-});
